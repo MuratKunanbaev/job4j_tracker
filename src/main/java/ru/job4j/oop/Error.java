fecdb4e38a2b2981ce2479c13ab0c9f347f98f01
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-04-17 01:36:01 +0500

    1.7. Конструктор [#501328]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,30 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+    public Error(){
+    }
+    public Error(boolean active, int status, String message){
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+    public void printInfo(){
+        System.out.println("Ошибка действующая: " + active);
+        System.out.println("Код ошибки: " + status);
+        System.out.println("Сообщение об ошибке: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printInfo();
+        Error notFound = new Error(true, 404, "Not Found");
+        notFound.printInfo();
+        Error badGateway = new Error(true, 502,  "Bad Gateway");
+        badGateway.printInfo();
+        Error notModified = new Error(true, 304,"Not Modified");
+        notModified.printInfo();
+    }
+}
