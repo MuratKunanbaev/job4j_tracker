f5677ddae1ecf5ff0dc428f50e80c836cfae4c0f
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-04-22 02:10:07 +0500

    2. Модель данных. [#501340]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+import java.time.LocalDate;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFio("Ivanov Ivan Ivanovich");
+        student.setGroupNumber("192");
+        LocalDate date = LocalDate.of(2009, 9, 1);
+        student.setReceiptDate(date);
+
+        System.out.println(student.getFio() + " studying in a group number " + student.getGroupNumber() +
+                " entered the University " + student.getReceiptDate());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String fio;
+    private String groupNumber;
+    private LocalDate receiptDate;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getGroupNumber() {
+        return groupNumber;
+    }
+
+    public void setGroupNumber(String groupNumber) {
+        this.groupNumber = groupNumber;
+    }
+
+    public LocalDate getReceiptDate() {
+        return receiptDate;
+    }
+
+    public void setReceiptDate(LocalDate receiptDate) {
+        this.receiptDate = receiptDate;
+    }
+}
