05897272f4abb206008212dc146b0cc6a8b2e02d
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-04-24 01:59:28 +0500

    9. toString [#501348]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,11 +5,9 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item itemFirst = new Item("Заявка1");
-        tracker.add(itemFirst);
-        Item itemId = tracker.findById(itemFirst.getId());
-        System.out.println(itemId.getId() + " - " + itemId.getName());
+        Item itemFirst = new Item(11,"Заявка1");
+        System.out.println(itemFirst);
+
     }
 
 }
