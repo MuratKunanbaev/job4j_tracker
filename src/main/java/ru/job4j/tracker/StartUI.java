21dfa5f1467dbe9079b48bcd8f96bf5cfe365971
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-04-26 02:11:46 +0500

    2.8. Реализация класса StartUI. Вывод заявок по имени [#501280]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -55,8 +55,20 @@ public class StartUI {
                 Item item = tracker.findById(id);
                 if (item != null) {
                     System.out.println(item);
-                }else {
+                } else {
                     System.out.println("Заявка с введенным id: " + id + " не найдена.");
+                }
+            } else if (select == 5) {
+                System.out.println("=== Find items by name ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for(Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
                 }
             } else if (select == 6){
                 run = false;
