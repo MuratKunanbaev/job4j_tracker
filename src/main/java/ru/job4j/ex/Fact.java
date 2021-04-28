f177c90459454b3a4d4fd718af6228f9c730a9af
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-04-29 00:55:20 +0500

    0.3. Тестирование исключений с junit [#501291]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -16,4 +16,5 @@ public class Fact {
         }
         return rsl;
     }
+
 }
---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenLess0() {
+        Fact.calc(-1);
+    }
+
+    @Test
+    public void when5Then120(){
+        int rsl = Fact.calc(5);
+       assertThat(rsl, is(120));
+    }
+}
