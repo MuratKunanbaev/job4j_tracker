d6232bb51255866f1a0245a062a70e771332725c
MuratKunanbaev <kunanbaevmurat5@gmail.com>
2021-05-23 18:51:04 +0500

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,5 +9,18 @@ public class Profiles {
      return profiles.stream()
                .map(Profile::getAddress)
              .collect(Collectors.toList());
+    }
+    Comparator<Address> comparatorCity = new Comparator<Address>() {
+        @Override
+        public int compare(Address o1, Address o2) {
+            return o1.getCity().compareTo(o2.getCity());
+        }
+    };
+
+    public List<Address> sorted(List<Address> list, Comparator<Address> comp) {
+        return list.stream()
+                .sorted(comp)
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -45,4 +45,26 @@ public class ProfilesTest {
         expected.add(new Address("Volgograd", "Pugacheva", 47, 1));
         assertThat(rsl, is(expected));
     }
+
+    @Test
+    public void when2IdenticalProfiles() {
+        List<Profile> profiles = List.of(
+                new Profile("Fedor", "Smirnov", 65,
+                        new Address("St.Petersburg", "Pushkina", 6, 9)),
+                new Profile("Ivan", "Ivanov", 25,
+                        new Address("Moscow", "Lenina", 34, 2)),
+                new Profile("Petr", "Sidorov", 32,
+                        new Address("Samara", "Gagarina", 16, 3)),
+                new Profile("Ivan", "Ivanov", 25,
+                        new Address("Moscow", "Lenina", 34, 2))
+        );
+        Profiles prof = new Profiles();
+        List<Address> rsl = prof.collect(profiles);
+        rsl = prof.sorted(rsl, prof.comparatorCity);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Lenina", 34, 2));
+        expected.add(new Address("Samara", "Gagarina", 16, 3));
+        expected.add(new Address("St.Petersburg", "Pushkina", 6, 9));
+        assertThat(rsl, is(expected));
+    }
 }
